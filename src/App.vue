1c04089c9a67720d921017a2dc7fb42c9943cc0d
evoLonation <1838940019@qq.com>
2022-11-15 10:22:32 +0800

    删除app的样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <template>
-<!--  <img alt="Vue logo" src="./assets/logo.png">-->
-<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   <router-view/>
   <login-window/>
 </template>
@@ -13,18 +11,8 @@ export default {
   name: 'App',
   components: {
     LoginWindow,
-    // HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
