437581b253b2592e8c6ef5a3a67c1dc71f850769
evoLonation <1838940019@qq.com>
2022-11-23 20:32:18 +0800

    添加主页背景

---
+++ src/test.vue
@@ -0,0 +1,22 @@
+<template>
+<p>{{query}}</p>
+</template>
+
+<script>
+import {  useRoute } from 'vue-router'
+import {ref} from "vue";
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "test",
+  setup(){
+    const query = ref(useRoute().query);
+    return{
+      query
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
