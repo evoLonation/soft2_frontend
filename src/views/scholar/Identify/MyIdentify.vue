4cbde56c5e145c23b37c65bc10723b03b72b2f07
XingruJiang <boxuan10258@qq.com>
2022-11-30 19:07:33 +0800

    jxr: 完成了学者认证部分

--- src/views/scholar/Identify/MyIdentify.vue
+++ src/views/scholar/Identify/MyIdentify.vue
@@ -1,18 +1,67 @@
 <template>
-  <id-view/>
+  <div class="identify">
+    <el-steps :active="active" align-center style="padding-top: 50px">
+      <el-step title="选择方式" icon="Edit" @click="reset" style="cursor: pointer"/>
+      <el-step title="填写内容" icon="Upload"/>
+      <el-step title="认证完成" icon="Picture"/>
+    </el-steps>
+    <select-view @activate="branch_activate" v-show="active === 1"/>
+    <mail-view ref="mail" @activate="activate" v-if="active === 2 && branch === 1"/>
+    <id-view ref="id" @activate="activate" v-if="active === 2 && branch === 2"/>
+    <success-view v-show="active === 3"/>
+  </div>
 </template>
 
 <script>
-import idView from "./ID_upload/ID_upload"
+import selectView from "./Selection/MySelect"
+import idView from "./ID_upload/MainWnd"
+import successView from "./Success/MySuccess"
+import mailView from "./Mail/MyMail"
 
 export default {
   name: "MyIdentify",
   components: {
-    idView
+    idView,
+    selectView,
+    successView,
+    mailView
   },
+  data() {
+    return {
+      bounce: true,
+      active: 1,
+      branch: -1,
+    }
+  },
+  methods: {
+    branch_activate(select) {
+      this.branch = select;
+      this.activate();
+      console.log(this.active, this.branch)
+    },
+    activate() {
+      if(this.active < 3) {
+        this.active = this.active + 1;
+      }
+    },
+    reset() {
+      if(this.active === 2) {
+        this.active = 1;
+        this.branch = -1;
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.identify {
+  width: 800px;
+  min-width: 800px;
+  max-width: 800px;
+  height: 500px;
+  margin: 100px auto auto 450px;
+  border-radius: 10px;
+  box-shadow: 0 0 4px rgba(0,0,0,0.40),0 0 6px rgba(0,0,0,0.15);
+}
 </style>
---
+++ src/views/scholar/Identify/Selection/MySelect.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="selection">
+    <el-card class="selection-card" @click="changeFather(1)">
+      <el-icon class="select-icon"><Message/></el-icon>
+      <p class="select-content">邮箱认证</p>
+    </el-card>
+    <el-card class="selection-card" @click="changeFather(2)">
+      <el-icon class="select-icon"><CreditCard/></el-icon>
+      <p class="select-content">身份认证</p>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import {Message} from "@element-plus/icons";
+import {CreditCard} from "@element-plus/icons";
+export default {
+  name: "MySelect",
+  components: {CreditCard, Message},
+  methods: {
+    changeFather(select) {
+      this.$emit('activate', select);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.selection {
+  display: flex;
+}
+
+.selection-card {
+  width: 323px;
+  height: 200px;
+  box-shadow: none;
+  border: 1.5px solid lightgrey;
+  margin: 70px 0 auto 50px;
+  cursor: pointer;
+  border-radius: 10px;
+  display: inline-block;
+}
+
+.selection-card:hover {
+  box-shadow: 0 0 4px rgba(0,0,0,0.15),0 0 6px rgba(0,0,0,0.08);;
+}
+
+.select-icon {
+  font-size: 45px;
+  color: #79bbff;
+  width: 100%;
+  height: 45px;
+  margin-top: 25px;
+}
+
+.select-content {
+  font-weight: 700;
+  text-align: center;
+}
+</style>
